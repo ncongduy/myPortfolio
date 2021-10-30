6ea46ca97a3bfd51e63c4a3e43e1e7e4010bf1ab
Nguyen Cong Duy <ncongduy@gmail.com>
2021-10-31 00:11:26 +0300

    add 5 projects Udacity

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,9 +2,10 @@ import { React, useState, useEffect } from 'react';
 import './portfolio.scss';
 import {
 	list,
-	htmlCssPortfolio,
-	javascriptPortfolio,
-	reactPortfolio,
+	udacityPortfolio,
+	codecademyPortfolio,
+	f8Portfolio,
+	pluralsightPortfolio,
 } from '../../data';
 import { Container } from './Container/Container';
 import { PortfolioList } from './PortfolioList/PortfolioList';
@@ -15,23 +16,26 @@ export default function Portfolio() {
 
 	useEffect(() => {
 		switch (selected) {
-			case 'html&css':
-				setData(htmlCssPortfolio);
+			case 'udacity':
+				setData(udacityPortfolio);
 				break;
-			case 'javascript':
-				setData(javascriptPortfolio);
+			case 'codecademy':
+				setData(codecademyPortfolio);
 				break;
-			case 'react':
-				setData(reactPortfolio);
+			case 'f8':
+				setData(f8Portfolio);
+				break;
+			case 'pluralsight':
+				setData(pluralsightPortfolio);
 				break;
 			default:
-				setData(htmlCssPortfolio);
+				setData(udacityPortfolio);
 		}
 	}, [selected]);
 
 	return (
-		<div className="portfolio" id="portfolio">
-			<h1>My Learning</h1>
+		<div className='portfolio' id='portfolio'>
+			{/* <h1>My Learning</h1> */}
 
 			<PortfolioList
 				list={list}
@@ -43,5 +47,3 @@ export default function Portfolio() {
 		</div>
 	);
 }
-
-
--- src/data.js
+++ src/data.js
@@ -1,63 +1,95 @@
 export const list = [
 	{
-		id: 'html&css',
-		title: 'HTML & CSS',
+		id: 'udacity',
+		title: 'Udacity',
 	},
 	{
-		id: 'javascript',
-		title: 'JavaScript',
+		id: 'codecademy',
+		title: 'Codecademy',
 	},
 	{
-		id: 'react',
-		title: 'React',
+		id: 'f8',
+		title: 'F8',
+	},
+	{
+		id: 'pluralsight',
+		title: 'Pluralsight',
 	},
 ];
 
-export const htmlCssPortfolio = [
+export const udacityPortfolio = [
 	{
 		id: 1,
-		title: 'Project 1 - Udacity',
+		title: 'Personal Blog Website',
 		img: '/assets/img/project1-udacity.jpg',
 		link: 'https://ncongduy.github.io/personal-blog-website/homepage.html',
-	},		
-];
-
-export const javascriptPortfolio = [
-	{
-		id: 1,
-		title: 'Matching game',
-		img: '/assets/img/matching-game.png',
-		link: 'https://ncongduy.github.io/fend-project-memory-game/',
-	},	
+	},
 	{
 		id: 2,
-		title: 'Project 2 - Udacity',
+		title: 'Landing Page',
 		img: '/assets/img/project2-udacity.jpg',
 		link: 'https://ncongduy.github.io/landing-page-project2-udacity/',
 	},
+	{
+		id: 3,
+		title: 'Weather Journal App',
+		img: '/assets/img/project3-udacity.jpg',
+		link: 'https://weather-journal-app-project3.herokuapp.com/',
+	},
+	{
+		id: 4,
+		title: 'Evaluate News NLP',
+		img: '/assets/img/project4-udacity.jpg',
+		link: 'https://evaluate-news-nlp-project4.herokuapp.com/',
+	},
+	{
+		id: 5,
+		title: 'Travel App',
+		img: '/assets/img/project5-udacity.jpg',
+		link: 'https://travel-app-project5-udacity.herokuapp.com/',
+	},
+	{
+		id: 6,
+		title: 'Matching game',
+		img: '/assets/img/matching-game.png',
+		link: 'https://ncongduy.github.io/fend-project-memory-game/',
+	},
 ];
 
-export const reactPortfolio = [
+export const codecademyPortfolio = [
 	{
 		id: 1,
-		title: 'Jamming',
+		title: 'React - Jamming',
 		img: '/assets/img/react1.jpg',
 		link: 'https://ncongduy.github.io/jamming/',
 	},
 	{
 		id: 2,
-		title: 'Alarm clock',
-		img: '/assets/img/react2.jpg',
-		link: 'https://ncongduy.github.io/alarm-clock/',
-	},
-];
-
-export const reduxPortfolio = [
-	{
-		id: 1,
-		title: 'Redux Matching Memory',
+		title: 'Redux - Matching Memory',
 		img: '/assets/img/redux1.jpg',
 		link: 'https://ncongduy.github.io/redux-matching-memory/',
 	},
+	{
+		id: 3,
+		title: 'Redux - Expense Tracker',
+		// img: '/assets/img/redux1.jpg',
+		// link: 'https://ncongduy.github.io/redux-matching-memory/',
+	},
 ];
 
+export const f8Portfolio = [
+	{
+		id: 1,
+		title: 'W3_band',
+		img: '/assets/img/html-css-exercise1.jpg',
+		link: 'https://ncongduy.github.io/f8-shop/w3_band/index.html',
+	},
+	{
+		id: 2,
+		title: 'Music Player',
+		img: '/assets/img/music-player.jpg',
+		link: 'https://ncongduy.github.io/music-player/',
+	},
+];
+
+export const pluralsightPortfolio = [];
