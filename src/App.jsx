ab3924a348e20270f689346f644beb4280accb7f
Nguyen Cong Duy <ncongduy@gmail.com>
2022-03-03 22:19:23 +0700

    feat: using React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,20 @@
-import React, { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-import Contact from './components/contact/Contact';
-import Intro from './components/intro/Intro';
-import Menu from './components/menu/Menu';
-import Portfolio from './components/portfolio/Portfolio';
 import Topbar from './components/topbar/Topbar';
-import './app.scss';
-
-// use React Context
-export const OpenContext = React.createContext();
+import Intro from './components/intro/Intro';
+import Portfolio from './components/portfolio/Portfolio';
+import Contact from './components/contact/Contact';
 
 function App() {
-	const [menuOpen, setMenuOpen] = useState(false);
-
 	return (
-		<div className='app'>
-			<OpenContext.Provider
-				value={{
-					menuOpen,
-					setMenuOpen,
-				}}
-			>
-				<Topbar />
-				<Menu />
-				<Intro />
-				<Portfolio />
-				<Contact />
-			</OpenContext.Provider>
+		<div>
+			<Topbar />
+
+			<Routes>
+				<Route path='/' element={<Intro />} />
+				<Route path='/portfolio' element={<Portfolio />} />
+				<Route path='/contact' element={<Contact />} />
+			</Routes>
 		</div>
 	);
 }
