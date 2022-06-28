ce02f1def8d0d31747739c83b934ff2670c0869b
Nguyen Cong Duy <ncongduy@gmail.com>
2022-06-29 01:41:39 +0300

    chore: update linkedin

--- src/pages/contact/linkedin/LinkedInComponent.jsx
+++ src/pages/contact/linkedin/LinkedInComponent.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
-
 export const LinkedInComponent = () => {
 	return (
 		<div className="itemContainer">
 			<a
-				href="https://www.linkedin.com/in/duy-nguyen-55b850197/"
+				href="https://www.linkedin.com/in/duy-nguyen-cong-55b850197/"
 				target="blank"
 				className="itemBox"
 			>
