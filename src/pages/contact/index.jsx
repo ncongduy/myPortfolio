3debd7ec8c0bdab56e7e5129f30a5221bd9cda78
Nguyen Cong Duy <ncongduy@gmail.com>
2022-07-03 12:09:14 +0300

    refactor: Contact component

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,26 +1,44 @@
-import React from 'react';
+import EmailIcon from '@mui/icons-material/Email';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
 import './contact.scss';
-import { MailComponent } from './mail/MailComponent';
-import { LinkedInComponent } from './linkedin/LinkedInComponent';
-import { GitHubComponent } from './github/GitHubComponent';
 
 export default function Contact() {
-	return (
-		<div className='contact' id='contact'>
-			<div className='contactImg'>
-				<img
-					src={process.env.PUBLIC_URL + '/assets/img/contact.jpg'}
-					alt='contact'
-				/>
-			</div>
+  return (
+    <div className="contact" id="contact">
+      <div className="contactImg">
+        <img src={process.env.PUBLIC_URL + '/assets/img/contact.jpg'} alt="contact" />
+      </div>
 
-			<div className='boxContainer'>
-				<div className='boxComponent'>
-					<MailComponent />
-					<LinkedInComponent />
-					<GitHubComponent />
-				</div>
-			</div>
-		</div>
-	);
+      <div className="boxContainer">
+        <div className="boxComponent">
+          <div className="itemContainer">
+            <div className="itemBox">
+              <EmailIcon className="icon" />
+              <span>ncongduy@gmail.com</span>
+            </div>
+          </div>
+
+          <div className="itemContainer">
+            <a
+              href="https://www.linkedin.com/in/duy-nguyen-cong-55b850197/"
+              target="blank"
+              className="itemBox"
+            >
+              <LinkedInIcon className="icon" />
+              <span>LinkedIn</span>
+            </a>
+          </div>
+
+          <div className="itemContainer">
+            <a href="https://github.com/ncongduy" target="blank" className="itemBox">
+              <GitHubIcon className="icon" />
+              <span>GitHub</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
